64c0953c04b60bd18b5c7ad76ff3cbcc57a46fea
Juan Capellan <juanluiscapellan1@gmail.com>
2022-02-14 21:32:39 -0400

    resolve some issue

--- src/services/Jwtservice.js
+++ src/services/Jwtservice.js
@@ -2,19 +2,19 @@ const config = require('../config');
 const jwt = require('jsonwebtoken');
 
 async function generateAccessToken(user) {
-  return accessToken = jwt.sign(user, config.ACCESS_TOKEN_SECRET, {
+  return jwt.sign(user, config.ACCESS_TOKEN_SECRET, {
     expiresIn: '6h',
-  });;
+  });
 }
 
 async function generateRefreshToken(user) {
-  return refreshToken = jwt.sign(user, config.REFRESH_TOKEN_SECRET, {
+  return jwt.sign(user, config.REFRESH_TOKEN_SECRET, {
     expiresIn: '1d',
   });
 }
 
 async function verifyRefreshToken(refreshToken) {
-  return verification = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
+  return jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
 }
 
 module.exports = {
