ea1293a7f0beb6b50d7473178362513270205ad3
Juan Capellan <juanluiscapellan1@gmail.com>
2022-03-02 16:30:39 -0400

    improve feature

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 80,
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
-  DB_WITH_URL: process.env.DB_CONN_URL || 'mongodb://localhost/test',
+  DB_WITH_URL: process.env.DB_WITH_URL || 'mongodb://localhost/test',
   ADMIN_RESET_SECRET: process.env.ADMIN_RESET_SECRET,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
